72c5adeb752fb8968726eca2cc88072328773b4c
jelicastanojevic <jelicastanojevicc@gmail.com>
2017-03-15 15:37:37 +0100

    Dodato ObrisiKnjigu

--- Biblioteka2017/src/biblioteka/interfejs/BibliotekaInterfejs.java
+++ Biblioteka2017/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -7,6 +7,7 @@ import biblioteka.Knjiga;
 
 public interface BibliotekaInterfejs {
 	public void dodajKnjigu(Knjiga k);
+	public void obrisiKnjigu(Knjiga k);
 	public LinkedList<Knjiga> vratiSveKnjige();
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, long isbn, String izdavac);
 }
