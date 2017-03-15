0b0c1a4a4c488e0c60b414ed3bfb7d382995bf9e
jelicastanojevic <jelicastanojevicc@gmail.com>
2017-03-15 15:33:24 +0100

    Uveden interfejs BibliotekaInterfejs

---
+++ Biblioteka2017/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -0,0 +1,12 @@
+package biblioteka.interfejs;
+
+import java.util.LinkedList;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
+public interface BibliotekaInterfejs {
+	public void dodajKnjigu(Knjiga k);
+	public LinkedList<Knjiga> vratiSveKnjige();
+	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, long isbn, String izdavac);
+}
